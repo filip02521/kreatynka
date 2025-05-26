366e5e635b6795f59f6b081881f19fcbbd8118e1
Filip Masny <Filip@MacBook-Pro.local>
2025-05-26 20:20:57 +0200

    Pierwszy commit - Kreatynka

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2F4F4F',
+        accent: '#FF8C00',
+        background: '#F5F5F5'
+      }
+    }
+  },
+  plugins: []
+};
