49ec4560a47875488f3c23241a08599fb507e603
Filip Masny <Filip@MacBook-Pro.local>
2025-05-26 21:58:31 +0200

    Aktualizacja projektu – nowe pliki, nowe logo itd.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 export default defineConfig({ integrations: [tailwind()] });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,7 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        primary: '#2F4F4F',
-        accent: '#FF8C00',
-        background: '#F5F5F5'
-      }
-    }
+    extend: {}
   },
   plugins: []
 };
